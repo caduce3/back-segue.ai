ff7e632f8421ae4605ecac64c7a4f0127dd79ef6
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-11-28 12:49:19 -0300

    feat: controller e rota para pegar os usuários da equipe dirigente

---
+++ src/http/controllers/_equipe-dirigente/pegar-users-equipe-dirigente.ts
@@ -0,0 +1,51 @@
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+import { ErroAoCarregarPagina } from "@/use-cases/@errors/erro-carregar-pagina";
+import { IgrejaNaoExiste } from "@/use-cases/@errors/igreja/erro-igreja-nao-existe";
+import { ErroVoceSoPodeRealizarUmaAcaoParaSuaIgreja } from "@/use-cases/@errors/transaction/erro-deletar-transaction-sua-igreja";
+import { makePegarUsersEquipeDirigenteUseCase } from "@/use-cases/@factories/equipe-dirigente/make-pegar-users-equipe-dirigente-use-case";
+import { ErroAoCarregarUsersEquipeDirigente } from "@/use-cases/@errors/equipeDirigente/erro-carregar-users-equipe-dirigente";
+
+export async function pegarUsersEquipeDirigente(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const pegarUsersEquipeDirigenteBodySchema = z.object({
+    page: z.number().positive(),
+    igrejaId: z.string(),
+    idUserEquipeDirigente: z.string(),
+  });
+
+  const { page, igrejaId, idUserEquipeDirigente } =
+    pegarUsersEquipeDirigenteBodySchema.parse(request.body);
+
+  try {
+    const pegarUsersEquipeDirigenteUseCase =
+      makePegarUsersEquipeDirigenteUseCase();
+
+    const { equipeDirigenteList, totalItens, totalPages } =
+      await pegarUsersEquipeDirigenteUseCase.execute({
+        page,
+        igrejaId,
+        idUserEquipeDirigente,
+      });
+
+    return reply.status(200).send({
+      totalItens,
+      totalPages,
+      currentPage: page,
+      equipeDirigenteList,
+    });
+  } catch (error) {
+    if (
+      error instanceof ErroAoCarregarUsersEquipeDirigente ||
+      error instanceof ErroAoCarregarPagina ||
+      error instanceof IgrejaNaoExiste ||
+      error instanceof ErroVoceSoPodeRealizarUmaAcaoParaSuaIgreja
+    ) {
+      return reply.status(409).send({ message: error.message });
+    }
+
+    throw error;
+  }
+}
--- src/http/controllers/routes/equipe-dirigente-routes.ts
+++ src/http/controllers/routes/equipe-dirigente-routes.ts
@@ -3,11 +3,18 @@ import { FastifyInstance } from "fastify";
 import { cadastrarUserEquipeDirigente } from "../_equipe-dirigente/cadastrar-user-equipe-dirigente";
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
 import { verificarPasta } from "@/http/middlewares/verificar-pasta";
+import { pegarUsersEquipeDirigente } from "../_equipe-dirigente/pegar-users-equipe-dirigente";
 
 export async function equipeDirigenteRoutes(app: FastifyInstance) {
   app.post(
     "/cadastrar_equipe_dirigente",
     { onRequest: [verifyJwt, verificarPasta(["PAROQUIA", "PADRE"])] },
     cadastrarUserEquipeDirigente
+  );
+
+  app.post(
+    "/pegar_equipe_dirigente",
+    { onRequest: [verifyJwt, verificarPasta(["FINANCAS", "PAROQUIA", "PADRE"])] },
+    pegarUsersEquipeDirigente
   );
 }
