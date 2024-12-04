e6f76cbb02a9ef1f59768b42b9d466dbb6b0a7d7
Carlos Eduardo <74209989+caduce3@users.noreply.github.com>
2024-12-04 16:47:36 -0300

    feat: interface e consulta para para pegar as fichas

--- src/repositories/ficha-repository.ts
+++ src/repositories/ficha-repository.ts
@@ -1,9 +1,28 @@
-import { Prisma, Ficha } from '@prisma/client'
+import { Prisma, Ficha } from "@prisma/client";
 
 export interface FichaRepository {
-    cadastrarFicha(data: Prisma.FichaCreateInput): Promise<Ficha>
-    findFichaByEmail(email: string): Promise<Ficha | null>
-    findFichaById(id: string): Promise<Ficha | null>
-    deletarFicha(id: string): Promise<Ficha | null>
-    atualizarFicha(id: string, data: Prisma.FichaUncheckedUpdateInput ): Promise<Ficha>
+  cadastrarFicha(data: Prisma.FichaCreateInput): Promise<Ficha>;
+  findFichaByEmail(email: string): Promise<Ficha | null>;
+  findFichaById(id: string): Promise<Ficha | null>;
+  deletarFicha(id: string): Promise<Ficha | null>;
+  atualizarFicha(
+    id: string,
+    data: Prisma.FichaUncheckedUpdateInput
+  ): Promise<Ficha>;
+  pegarFichas(
+    take: number,
+    page: number,
+    igrejaId: string,
+    nomePastaFichas?: string,
+    nomeJovem?: string,
+    anoEncontro?: string,
+    corCirculoOrigem?: string
+  ): Promise<{
+    fichas: Prisma.FichaGetPayload<{
+      include: {
+        FichaEquipe: true;
+      };
+    }>[];
+    totalCount: number;
+  }>;
 }
--- src/repositories/prisma/prisma-ficha-repository.ts
+++ src/repositories/prisma/prisma-ficha-repository.ts
@@ -1,4 +1,4 @@
-import { Prisma, Ficha } from "@prisma/client";
+import { Prisma, Ficha, CoresCirculos } from "@prisma/client";
 import { prisma } from "@/lib/prisma";
 import { FichaRepository } from "../ficha-repository";
 
@@ -53,5 +53,72 @@ export class PrismaFichaRepository implements FichaRepository {
     });
 
     return ficha;
+  }
+
+  async pegarFichas(
+    take: number,
+    page: number,
+    igrejaId: string,
+    nomePastaFichas?: string,
+    nomeJovem?: string,
+    anoEncontro?: string,
+    corCirculoOrigem?: string
+  ): Promise<{
+    fichas: Prisma.FichaGetPayload<{
+      include: {
+        FichaEquipe: true;
+      };
+    }>[];
+    totalCount: number;
+  }> {
+    const skip = (page - 1) * take;
+
+    // Construindo as condições dinamicamente
+    const conditions: Prisma.FichaWhereInput[] = [];
+
+    if (nomePastaFichas)
+      conditions.push({
+        nomePastaFichas: { contains: nomePastaFichas, mode: "insensitive" },
+      });
+    if (nomeJovem)
+      conditions.push({
+        nomeJovem: { contains: nomeJovem, mode: "insensitive" },
+      });
+    if (anoEncontro)
+      conditions.push({
+        anoEncontro: { contains: anoEncontro, mode: "insensitive" },
+      });
+    if (corCirculoOrigem)
+      conditions.push({
+        corCirculoOrigem: { equals: corCirculoOrigem as CoresCirculos },
+      });
+
+    //adicionar a condição do igrejaId
+    conditions.push({ igrejaId: igrejaId });
+
+    // Garantindo que só passemos o AND se tivermos condições
+    const whereClause: Prisma.FichaWhereInput =
+      conditions.length > 0 ? { AND: conditions } : {};
+
+    const totalCount = await prisma.ficha.count({
+      where: whereClause,
+    });
+
+    const fichas = await prisma.ficha.findMany({
+      where: whereClause,
+      orderBy: {
+        createdAt: "desc",
+      },
+      include: {
+        FichaEquipe: true,
+      },
+      take,
+      skip,
+    });
+
+    return {
+      fichas,
+      totalCount,
+    };
   }
 }
